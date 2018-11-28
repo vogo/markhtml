ec895420e3e492e63b6ef530b1ef2d179b61808d
wangoo <wangoo@soundbus.cn>
2018-11-28 09:36:28 +0800

    fasthttp

--- main.go
+++ main.go
@@ -1,34 +1,28 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/valyala/fasthttp"
 )
 
 var (
-	port            = flag.String("port", "80", "port")
 	markhtmljsBytes = []byte(Files["markhtml.min.js"])
 	indexhtmlBytes  = []byte(Files["index.min.html"])
 )
 
+func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	if string(ctx.Path()) == "/markhtml.js" {
+		ctx.SetContentType("application/javascript")
+		ctx.SetBody(markhtmljsBytes)
+		return
+	}
+	ctx.SetContentType("text/html")
+	ctx.SetBody(indexhtmlBytes)
+}
+
 func main() {
-	flag.Parse()
-	fmt.Printf("start server on port %s", *port)
-
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.New()
-
-	r.GET("/*.html", func(c *gin.Context) {
-		if c.Request.URL.Path == "/markhtml.js" {
-			c.Data(http.StatusOK, "application/javascript", markhtmljsBytes)
-			return
-		}
-		c.Data(http.StatusOK, "text/html", indexhtmlBytes)
-	})
-
-	r.Run(fmt.Sprintf(":%s", *port))
-
+	fmt.Printf("start markhtml")
+	fasthttp.ListenAndServe(":80", fastHTTPHandler)
 }
