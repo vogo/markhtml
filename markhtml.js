20c2cdb52ded07aaa997bce59e2b8cadb5a202af
wangoo <wangoo@soundbus.cn>
2018-11-30 17:03:01 +0800

    support input markdown url

--- markhtml.js
+++ markhtml.js
@@ -1,30 +1,63 @@
+function dom(id) {
+    return document.getElementById(id)
+}
+
+function ndom(n) {
+    return document.createElement(n)
+}
+
+function endwith(s, c) {
+    return s.indexOf(c, s.length - c.length) != -1;
+}
+
+function hformt(text) {
+    return text
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+}
 (function(level) {
+
     var mh = {
         level: level,
+
         v: function(s) {
             if (s == "") {
-                s = "<h1>404!</h1>找不到你要访问的页面!";
+                s = "找不到你要访问的页面!";
             }
-            app = document.getElementById('app');
+            app = dom('app');
             app.innerHTML = s;
             h1 = document.getElementsByTagName("h1");
+            mainTitle = ""
             if (h1.length > 0) {
                 mainTitle = h1[0];
-                document.title = mainTitle.innerText;
-                document.body.insertBefore(this.buildmenu(), app)
+                mh.buildmenu()
             }
+            if (mainTitle == "") {
+                mainTitle = "Markdown To HTML"
+            }
+            document.title = mainTitle.innerText;
             if (callbackAfterShow && typeof callbackAfterShow == "function") {
                 callbackAfterShow()
             }
         },
-
         loadmark: function() {
             url = window.location.pathname;
-            if (url.length < 6 || url.indexOf(".html", url.length - 5) == -1) {
-                mh.v("");
+            if (url.length < 10) {
+                dom("editor").style.display = ""
+                return
+            }
+            url = "http://" + url
+            if (!endwith(url, ".markdown") && !endwith(url, ".md")) {
+                mh.v("不支持的markdown地址: " + url);
                 return;
             }
-            url = "http://doc.sisopipo.com/" + url.substr(0, url.length - 5) + ".md";
+            mh.loadurl(url)
+        },
+        loadurl: function(url) {
+            dom("menu").innerHTML = ""
             if (window.XMLHttpRequest) {
                 xmlhttp = new XMLHttpRequest();
             } else {
@@ -33,43 +66,47 @@
             xmlhttp.onreadystatechange = function() {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                     mh.v(marked(xmlhttp.responseText));
+                    return
+                }
+                if (xmlhttp.status == 404) {
+                    mh.v("")
+                    return
+                }
+                if (xmlhttp.status >= 400) {
+                    mh.v("无法加载请求页面")
                 }
             }
-            xmlhttp.onerror = function() {
-                mh.v("")
+            xmlhttp.onerror = function(err) {
+                mh.v(err)
             };
             xmlhttp.open("GET", url, true);
             xmlhttp.send();
         },
         buildmenu: function() {
-            var m = document.createElement('div')
-            m.style.cssText = "float:left;position:fixed;"
-            var content = document.querySelector('.markdown-body');
+            var html = dom("app").innerHTML;
+            var pattern = new RegExp("<h([1-" + mh.level + "]) id=\"([^\"]+)\"", "g");
 
-            var html = content.innerHTML;
-            var pattern = /<h([1-3]) id="([^"]+)">/g
-
-
-            var previousLi = m;
+            var previousLi = dom("menu");
             var previousLevel = 0;
             var previousUl;
 
             while ((matchArr = pattern.exec(html))) {
                 level = parseInt(matchArr[1])
                 id = matchArr[2]
-                console.log(level + ":" + id)
-                h = document.getElementById(id)
+                h = dom(id)
                 link = mh.makeLink(h)
 
                 var ul;
 
                 if (level > previousLevel) {
-                    ul = document.createElement('ul')
+                    ul = ndom('ul')
                     previousLi.appendChild(ul)
                 } else if (level == previousLevel) {
                     ul = previousUl;
                 } else {
-                    ul = previousUl.parentNode.parentNode
+                    while (level < previousLevel--) {
+                        ul = previousUl.parentNode.parentNode
+                    }
                 }
                 ul.appendChild(link)
 
@@ -77,12 +114,10 @@
                 previousUl = ul
                 previousLevel = level
             }
-
-            return m
         },
 
         makeLink: function(h) {
-            var link = document.createElement('li')
+            var link = ndom('li')
             window.arst = h
             var text = [].slice.call(h.childNodes).map(function(node) {
                 if (node.nodeType === Node.TEXT_NODE) {
@@ -95,18 +130,11 @@
             }).join('').replace(/\(.*\)$/, '')
             link.innerHTML =
                 '<a class="section-link" data-scroll href="#' + h.id + '">' +
-                mh.htmlEscape(text) +
+                hformt(text) +
                 '</a>'
             return link
         },
-        htmlEscape: function(text) {
-            return text
-                .replace(/&/g, '&amp;')
-                .replace(/"/g, '&quot;')
-                .replace(/'/g, '&#39;')
-                .replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;')
-        }
+
     };
 
     window.markhtml = mh;
