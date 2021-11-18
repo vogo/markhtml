7601b895360bd5140d13806f682a34dd1857e100
wangoo <wongoo@apache.org>
2021-11-18 15:08:34 +0800

    hidden mindmap raw data

--- markmind.js
+++ markmind.js
@@ -1,17 +1,18 @@
 function markmind() {
-    let minds = document.getElementsByClassName("language-mindmap");
-    for (let i = 0; i < minds.length; i++) {
-        let mind = minds[i];
-        let data = mind.innerText;
-        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("id", "mindmap_" + i);
-        svg.setAttribute('style', 'width: 100%;')
-        let preTagNode = mind.parentNode;
-        preTagNode.parentNode.insertBefore(svg, preTagNode.nextSibling);
-        renderMindmap(svg, data);
-    }
+  let minds = document.getElementsByClassName("language-mindmap");
+  for (let i = 0; i < minds.length; i++) {
+    let mind = minds[i];
+    let data = mind.innerText;
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("id", "mindmap_" + i);
+    svg.setAttribute('style', 'width: 100%;')
+    let preTagNode = mind.parentNode;
+    preTagNode.parentNode.insertBefore(svg, preTagNode);
+    renderMindmap(svg, data);
+    preTagNode.style.display = 'none';
+  }
 }
 
-loadScript("", "/lib/markmind_render.js", function () {
-    markmind();
+loadScript("", "/lib/markmind_render.js", function() {
+  markmind();
 })
